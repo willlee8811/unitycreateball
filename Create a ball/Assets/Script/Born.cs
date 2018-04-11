999a1f6032b89950e93a7f7a689e1fbd300a5b50
LiaoYiZheng <410121038@ems.ndhu.edu.tw>
2018-04-11 11:22:18 +0800

    加入專案

---
+++ Create a ball/Assets/Script/Born.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Born : MonoBehaviour {
+
+    public GameObject ball;
+    GameObject newBall;
+    float timer_f = 0f;
+    int timer_i = 0;
+	// Use this for initialization
+	void Start () {
+        CreateBall();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        timer_f += Time.deltaTime;
+        timer_i = (int)timer_f;
+        if(timer_i == 3)
+        {
+            if (newBall != null)
+                GameObject.Destroy(newBall);
+            CreateBall();
+            timer_f = 0;
+        }
+	}
+    void CreateBall()
+    {
+        newBall = GameObject.Instantiate<GameObject>(ball);
+        newBall.transform.position = new Vector3(0, 10, 0);
+    }
+}
